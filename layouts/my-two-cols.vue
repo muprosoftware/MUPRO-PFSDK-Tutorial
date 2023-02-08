d1eb51de15fc68a20b48dc091d81d3d162bb3cc7
cxx <0001001bill@gmail.com>
2023-02-07 21:05:34 -0600

    add a two column layout with title

---
+++ layouts/my-two-cols.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <div class="w-full h-full slidev-layout">
+    <slot name="title"/>
+    <div class="two-columns grid grid-cols-2 mt-0">
+      <div class="col-left" :class="props.class">
+        <slot name="left"/>
+      </div>
+      <div class="col-right" :class="props.class">
+        <slot name="right" />
+    </div>
+  </div>
+</div>
+</template>
